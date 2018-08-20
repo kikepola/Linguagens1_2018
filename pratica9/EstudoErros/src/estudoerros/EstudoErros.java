1de1fe8a3935f5c1a4eb7fda7bec312dfdd5bc01
kikepola <kikepola@gmail.com>
2018-08-20 10:30:39 -0300

    Pratica 9

---
+++ pratica9/EstudoErros/src/estudoerros/EstudoErros.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package estudoerros;
+
+/**
+ *
+ * @author 16.00026-9
+ */
+public class EstudoErros {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Chamada na Main:");
+        metodo1();
+        System.out.println("Fim da Main");
+    }
+
+    static void metodo1() {
+        System.out.println("Chamada Metodo 1:");
+        metodo2();
+        System.out.println("Fim do Metodo 1");
+    }
+
+    private static void metodo2() {
+        System.out.println("Chamada na Metodo 2:");
+        int [] array = new int[10];
+        for(int i = 0; i < 15; i++){
+            array[i] = i;
+            System.out.println(""+ i);
+        }
+        System.out.println("Fim do Metodo 2");
+    }
+    
+}
